5bfe57018f6bbde8427d081b6c27a51529fa6e7b
yarogniew <pan@yarogniew.net>
2018-05-28 22:48:18 +0200

    CtlScan(pp, True)
    adding some comments

--- examples/utilities/02_midi_ctl_scan.py
+++ examples/utilities/02_midi_ctl_scan.py
@@ -5,25 +5,30 @@ Scan for Midi controller numbers. Launch this script from a terminal.
 
 """
 from pyo import *
-import time, sys
+import time
+import sys
 
 if sys.version_info[0] < 3:
-    input = raw_input
+    input = raw_input    # if Python 2.x use a raw_input instead of an input
 
-pm_list_devices()
+pm_list_devices()  # Prints a list of all devices found by Portmidi.
 
-num = eval(input("Enter your Midi interface number : "))
+num = eval(input("Enter your Midi interface number: "))
+
+ins = pm_get_input_devices()
+print(f"selected ---> {ins[0][num]}\n")
 
 s = Server(duplex=0)
-s.setMidiInputDevice(num)
+s.setMidiInputDevice(num)  # open num device
 s.boot().start()
 
 print("Play with your Midi controllers...")
 
-def pp(x): 
-    print("controller number =", x)
+def pp(x):
+    pass
+    # print("controller number =", x)
 
-scan = CtlScan(pp, False)
+scan = CtlScan(pp, True)   # Scan the Midi controller’s number in input.
 
 again = "y"
 while again == "y":
